685f3bf615fa20f95634f75ac56c7df656c0ad37
KSK9820 <kimmmuo12@gmail.com>
2020-12-29 22:02:36 +0900

    구글 로그인 화면으로 사용 할 Login.class와 activity_login.xml 새로 생성, manifest에 제일 첫 화면을 activity_login으로 변경

---
+++ app/src/main/java/com/example/myapplication5/Login.java
@@ -0,0 +1,31 @@
+package com.example.myapplication5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button imageButton = (Button) findViewById(R.id.btn1);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+}
