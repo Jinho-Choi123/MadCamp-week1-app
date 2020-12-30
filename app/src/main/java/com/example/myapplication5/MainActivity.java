c886258537eb621acc452dbb4cc1e53ddcd29e12
KSK9820 <kimmmuo12@gmail.com>
2020-12-30 14:14:29 +0900

    google API 연동 로그인 기능 추가

--- app/src/main/java/com/example/myapplication5/MainActivity.java
+++ app/src/main/java/com/example/myapplication5/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.myapplication5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView name, email, id;
+    Button signOut;
+    GoogleSignInClient mGoogleSignInClient;
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MainActivity.this, "Signed out successfully", Toast.LENGTH_LONG).show();
+                        finish();
+
+                    }
+                });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
         tabHost1.setup() ;
@@ -20,6 +56,38 @@ public class MainActivity extends AppCompatActivity {
         ts1.setContent(R.id.content1) ;
         ts1.setIndicator("연락처") ;
         tabHost1.addTab(ts1)  ;
+
+        imageView = findViewById(R.id.imageView);
+        name = findViewById(R.id.textName);
+        email=findViewById(R.id.textEmail);
+        id=findViewById(R.id.textID);
+        signOut=findViewById(R.id.sign_out_button);
+        signOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_out_button:
+                        signOut();
+                        break;
+                }
+            }
+        });
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+            name.setText(personName);
+            email.setText(personEmail);
+            id.setText(personId);
+            Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView);
+
+
+        }
+
+
 
         // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
         TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
@@ -34,4 +102,5 @@ public class MainActivity extends AppCompatActivity {
         tabHost1.addTab(ts3) ;
 
     }
+
 }
